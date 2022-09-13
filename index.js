6ecc2939102b78e02fa68a02475a97536c3b3c2f
Paul Thimote <pthimote@gmail.com>
2022-09-12 23:10:17 -0400

    commit tree 41994d204c6a6d9901e0a5fc3fcd45fe0aad0366

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(records){
+    const found = records.find(record =>{
+        console.log("record", record)
+        return record.result === 'W'
+    })
+
+        console.log('found', found)
+         if (found) {
+            return found.year
+         }
+}
